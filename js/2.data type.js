4190bf016d68b8da738364616f78a8bb9d7d5a12
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-27 01:53:58 +0530

    function expration

---
+++ js/2.data type.js
@@ -0,0 +1,28 @@
+// type premitive or nonpremitive
+// 1.number 2.string 3.boolean 4.undefined 5.null 6.symbol 7.big int
+// Rest are nonpremitive like array object and the function
+
+
+// premitive //parent
+// const num = 4;
+// const str = 'str';
+// const boolean = true;
+// const undef = undefined;
+// const nul = null;
+// const symbl = Symbol(); //uniqe
+const bigINt = BigInt;//return the large integer values
+// console.log(typeof bigINt)
+
+let a = 6726257467625742674674657467267467546745674672564756n;
+// console.log(a)
+
+// It's a king no one can compare to the king
+let smbl = Symbol('#$@Q#$')
+let smbl2 = Symbol('#$@Q#$')
+// console.log(smbl===smbl2)
+// nonpremetive
+const arr = [23,'34',true,null,undefined]
+console.log(arr)
+// const obj = {}
+// const fun = function(){}
+// console.log(typeof fun)
