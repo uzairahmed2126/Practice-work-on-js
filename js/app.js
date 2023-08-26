d79ac55cd06dfa1260353272889a6ba52d0f50b9
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-27 01:04:32 +0530

    .

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Arithmetic operator
+// 1.normal function
+// 2.arrow function,3.anonymous function,4. function declaration,5.naming function expration 6.call back
+
+// function name is a calling statement
+function normal(){
+    return true; //return statement
+}
+console.log(normal()) //Call function
