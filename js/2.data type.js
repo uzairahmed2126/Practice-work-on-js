ade3d4e8606d3c7b18df5e193345b0c5669fb559
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-27 02:02:29 +0530

    type

--- js/2.data type.js
+++ js/2.data type.js
@@ -20,9 +20,28 @@ let a = 6726257467625742674674657467267467546745674672564756n;
 let smbl = Symbol('#$@Q#$')
 let smbl2 = Symbol('#$@Q#$')
 // console.log(smbl===smbl2)
+
 // nonpremetive
 const arr = [23,'34',true,null,undefined]
 console.log(arr)
 // const obj = {}
 // const fun = function(){}
 // console.log(typeof fun)
+let arr23;
+console.log(arr23)
+
+// undeclare
+let = 89;
+console.log(let)
+
+// uninitilize
+a1=3 ;
+console.log(a1)
+
+
+// negative 0
+console.log(0 > -0)
+console.log(0 < -0);
+console.log(0 == -0);//It's not a true 0 is not equal to -0
+console.log(Object.is(0,-0));
+
