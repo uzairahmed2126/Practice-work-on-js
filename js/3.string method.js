4032ccd66b7c9cc4f166456d83e637f43b8ba6be
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-30 01:31:27 +0530

    string method

--- js/3.string method.js
+++ js/3.string method.js
@@ -11,24 +11,63 @@ let atMethod = 'uzair ahmed'
 let charAt = 'Adnan Belim'
 console.log(charAt.charAt(2))
 
-// charCodeat Return the ASCII or sky 
+// 4.charCodeat Return the ASCII or sky 
 let charCodeat = 'Fayya ur rehman'
 console.log(charCodeat.charCodeAt(1))
 
-// codePointat as work charCodeAt
+// 5.codePointat as work charCodeAt
 let codePointat = 'Oye chal chai pite'
 console.log(codePointat.codePointAt(6))
 
-// concat
+// 6.concat
 let firstName = 'Uzair'
 let LastName = 'Ahmed'
-console.log(firstName.concat('\n'+LastName));
+console.log(firstName.concat('\n' + LastName));
 
-// startwith
+// 7.startwith
 let startwith = 'escape sequence'
 console.log(startwith.startsWith('e'))
 
-// endwidth
+// 8.endwidth
 let endwith = 'end with'
 console.log(endwith.endsWith('h'))
+
+// 9.fromcharcode return the word from the ASCII or sky number
+let fromcharcode = 97
+console.log(String.fromCharCode(fromcharcode))
+
+// 10.includes return the boolean if value exist or not
+let Incld = 'hey'
+console.log(Incld.includes('hey'))
+
+// 11.IndexOf
+let IndexOf = 'My name is fayyaz'
+console.log(IndexOf.indexOf('fayyaz'))
+
+// 12.lastIndex
+let lastIndex = 'My name is Adnan My name is Adnan'
+console.log(lastIndex.lastIndexOf('Adnan'))
+
+// 13.localCompare it throw binary value
+let text1 = "ab";  //before
+let text2 = "fb";   //after
+let result = text1.localeCompare(text2);
+console.log(result)
+
+// 14.match return the character
+let Cartoon = 'My Fav Cartoon Is Doraemon'
+let secCartoon = /[a-z]/g
+console.log(Cartoon.match(secCartoon))
+
+// 15.normalization
+
+// 16.padstart
+let padstart = 'fayyaz'
+console.log(padstart.padStart(10,'k'));
+
+
+// 16.padend
+let padend = 'fayyaz'
+console.log(padend.padEnd(10,' k'));
+
 
