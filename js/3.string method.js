ffb73eb7a12cc89fcc653995704ee0a83cff8d9e
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-30 00:40:50 +0530

    string method

---
+++ js/3.string method.js
@@ -0,0 +1,20 @@
+// string target the 
+// 1.length
+let str = 'abcdefghijklmnopqrstuvwxyz'
+console.log(str.length)
+
+// 2.at metthod return the given index value
+let atMethod = 'uzair ahmed'
+// console.log(atMethod.at(2))
+
+// 3.charat same as at method
+let charAt = 'Adnan Belim'
+console.log(charAt.charAt(2))
+
+// charCodeat Return the ASCII or sky 
+let charCodeat = 'Fayya ur rehman'
+console.log(charCodeat.charCodeAt(1))
+
+// codePointat as work charCodeAt
+let codePointat = 'Oye chal chai pite'
+console.log(codePointat.codePointAt(6))
