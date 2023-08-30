863be9d6a6337f8874197a52fbc32d3d612a30a1
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-31 01:10:24 +0530

    array method

--- js/4.Array method.js
+++ js/4.Array method.js
@@ -1,13 +1,19 @@
 // 1.at method return the index value
-let arr = ['a','b','c']
+let arr = ['a', 'b', 'c']
 console.log(arr.at(0))
 
 // 2.length
-let arrLength = ['uzair','fayyaz']
+let arrLength = ['uzair', 'fayyaz']
 console.log(arrLength.length)
 
 // 3.concat concat the array
 let ar1 = ['uzair']
 let ar2 = ['ahmed']
 console.log(ar1.concat(ar2))
+
+// 4.copywithin 
+let copywithin = ['0', '1', '2', '3', '4', '5', '6', '7']
+console.log(copywithin.copyWithin(2,2,7))
+
+// 5.
 
