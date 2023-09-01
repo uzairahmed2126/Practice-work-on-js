27a5f37233ca57983d0ce57829d4d9a5d225ce86
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-02 00:58:25 +0530

    map

--- js/4.Array method.js
+++ js/4.Array method.js
@@ -1,43 +1,43 @@
-// // 1.at method return the index value
-// let arr = ['a', 'b', 'c']
+// 1.at method return the index value
+let arr = ['a', 'b', 'c']
 // console.log(arr.at(0))
 
-// // 2.length
-// let arrLength = ['uzair', 'fayyaz']
+// 2.length
+let arrLength = ['uzair', 'fayyaz']
 // console.log(arrLength.length)
 
-// // 3.concat concat the array
-// let ar1 = ['uzair']
-// let ar2 = ['ahmed']
+// 3.concat concat the array
+let ar1 = ['uzair']
+let ar2 = ['ahmed']
 // console.log(ar1.concat(ar2))
 
-// // 4.copywithin 
-// let copywithin = ['0', '1', '2', '3', '4', '5', '6', '7']
+// 4.copywithin 
+let copywithin = ['0', '1', '2', '3', '4', '5', '6', '7']
 // console.log(copywithin.copyWithin(2, 2, 7))
 
-// // 5.entries 
-// let entris = ['apple', 'banana', 'kivi', 'she said abba nhi manenge'];
-// let DilJaleAAsiq = entris.entries(entris)
+// 5.entries 
+let entris = ['apple', 'banana', 'kivi', 'she said abba nhi manenge'];
+let DilJaleAAsiq = entris.entries(entris)
 // console.log(DilJaleAAsiq.next().value);
 
-// // 6.every it is a callback function and it's throw true and false when it find less then given value is find or not it's depends
-// let arrr = [1, 2, 3, 4, 5, 6]
+// 6.every it is a callback function and it's throw true and false when it find less then given value is find or not it's depends
+let arrr = [1, 2, 3, 4, 5, 6]
 // console.log(arrr.every((a) => {
 //     return a < 5;
 // }))
 
-// // 7.some it is check the all value when it's find same then it's throw true niether false
-// let som = [1, 2, 3, 4, 5, 6]
+// 7.some it is check the all value when it's find same then it's throw true niether false
+let som = [1, 2, 3, 4, 5, 6]
 // console.log(som.some((a) => {
 //     return a < 5
 // }))
 
-// // 8.fill 
-// let fill = [0, 1, 2, 3, 4, 5, 6, 7]
+// 8.fill 
+let fill = [0, 1, 2, 3, 4, 5, 6, 7]
 // console.log(fill.fill('kayda', 2, 7))
 
-// // 9.filter method return the given value as it find
-// let filtermethod = [1, 2, 3, 4, 4, 6, 4, 3];
+// 9.filter method return the given value as it find
+let filtermethod = [1, 2, 3, 4, 4, 6, 4, 3];
 // console.log(filtermethod.filter((e) => {
 //     return e >= 4;
 // }))
@@ -88,7 +88,18 @@ let isarr = [13]
 
 // 19.join 
 let JOIN = ['adnan','fayyaz','uzair']
-console.log(JOIN.join(' '))
+// console.log(JOIN.join(' '))
 
 let num = [1,2,3,4]
-console.log(num.join('/'))
+// console.log(num.join('/'))
+
+// 20.lastIndexof
+let lastIndex = [1,2,3,4,5]
+// console.log(lastIndex.lastIndexOf(5))
+
+// 21.Map method it is a itirator return new array
+let MapM = [1,2,3,4,5,6,7,8]
+console.log(MapM.map((e)=>{
+    return e**2
+}))
+
