57d1c09b2ed2b8d727b640ffb73f732384fc44d3
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-12 03:40:33 +0530

    math

--- js/6.number method.js
+++ js/6.number method.js
@@ -3,12 +3,28 @@ let obj = {a:'123'};
 // console.log(Number(num))
 // console.log(Number(obj.a))
 
-// parsfloat
+// 1.parsfloat
 let float = 21.1
-console.log(Number.parseFloat(float))
+// console.log(Number.parseFloat(float))
 
-// parsint
+// 2.parsint
 let int = 1.2
-console.log(Number.parseInt(int))
-console.log(123===123.0)//true
+// console.log(Number.parseInt(int))
+// console.log(123===123.0)//true
+
+// 3.isfinite check the value is countable or not
+let finite = 2-139
+// console.log(Number.isFinite(finite))
+
+// 4.isInt
+let isint = 2.139
+// console.log(Number.isInteger(isint))
+
+// 5.tofix set the decimal value
+let tofix = 1.100
+// console.log(tofix.toFixed(50))
+
+// 6.Precision
+let Precision =12
+console.log(Precision.toPrecision(6))
 
---
+++ js/7.math method.js
@@ -0,0 +1,25 @@
+// 1.floor, up to down 
+let num = 63670.100
+// console.log(Math.floor(num))
+
+// 2.ceil, down to up
+let numceil = 63670.1
+// console.log(Math.ceil(numceil))
+
+// 3.round, if the value is gretter then it's throw upper value
+let rnd = 63670.6
+// console.log(Math.round(rnd))
+
+// 4.trunc, return the value in form of intezer
+let trunc  = 1234
+// console.log(Math.trunc(trunc))
+
+// 5.min
+console.log(Math.min(2,5,32))
+
+// 6.max
+console.log(Math.max(2,5,32))
+
+// 7.cueb
+console.log(Math.cbrt(90))
+
