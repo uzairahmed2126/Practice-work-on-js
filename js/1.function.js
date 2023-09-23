87628a1448c2de14dd17afe327d200ba03096030
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-24 00:49:00 +0530

    .

--- js/1.function.js
+++ js/1.function.js
@@ -1,7 +1,6 @@
 // scope and function expression kyle symption 3 module
 // Arithmetic operator
-// 1.normal function
-// 2.arrow function,3.anonymous function,4. function declaration,5.naming function expration 6.call back
+// 1.normal function 2.arrow function, 3.anonymous function, 4.function declaration, 5.naming function expration 6.call back
 
 
 // ==========
@@ -18,8 +17,8 @@ const arr = (a, b) => (a - b)
 console.log(arr(1, 3))
 
 
-const addition =(a,b,c)=>(a+b-c)
-console.log(addition(4,5,3))
+const addition = (a, b, c) => (a + b - c)
+console.log(addition(4, 5, 3))
 // ===========
 
 // ===========
@@ -43,7 +42,7 @@ const funExp = function (a, b) {
 // ===========
 
 // function naming
-const fnName= function(){
+const fnName = function () {
     console.log('fayyyaz')
 }
 fnName()
--- js/2.data type.js
+++ js/2.data type.js
@@ -11,12 +11,12 @@
 // const nul = null;
 // const symbl = Symbol(); //uniqe
 const bigINt = BigInt;//return the large integer values
-// console.log(typeof bigINt)
+// console.log(bigINt(23))
 
-let a = 6726257467625742674674657467267467546745674672564756n;
+let a = 6726257467625742674674657467267467546745674672564756n;//bigint
 // console.log(a)
 
-// It's a king no one can compare to the king
+// It's a uniqe no one can compare to the uniqe
 let smbl = Symbol('#$@Q#$')
 let smbl2 = Symbol('#$@Q#$')
 // console.log(smbl===smbl2)
@@ -45,18 +45,19 @@ a1=3 ;
 // console.log(0 == -0);//It's not a true 0 is not equal to -0
 // console.log(Object.is(0,-0));
 
-// NANisNAN //invalid number
+// isNAN //invalid number
 // console.log(isNaN(Number('asdfa')))
 
 // introduction kyle symption module 1
 // prefix and postfix increment and  decrement
 let prf = 2;
-console.log(++prf);
-console.log(--prf);
+// console.log(++prf+prf);//The answer is 6 because the first prefix value is 3 and now +prf's value is 3 so 3+3 is a 6.
+
+// console.log(--prf);
 
 let post = 3
-console.log(post++);
-console.log(post--);
-console.log(post);
+console.log(post++,post);//Answer will be 3,4 because the post fix would be execute but it is not throwing the right output we have to see the post output then we have to log the post again.
+// console.log(post--);
+// console.log(post);
 
 
