23328a828a79790f77f825d482a9b6c9914d257d
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-24 02:11:50 +0530

    2day

---
+++ days/2.day.js
@@ -0,0 +1,17 @@
+function giveMeSomething(str) {
+    // return "something "+str
+    // return str='something '+str
+    // return "something ".concat(str)
+    // return `Something ${str}`
+}
+console.log(giveMeSomething("is better than nothing"))
+console.log(giveMeSomething("Bob Jane"))
+console.log(giveMeSomething("something"))
+
+
+function triArea(base, hieght) {
+    return base * hieght / 2;
+}
+console.log(triArea(3, 2))
+console.log(triArea(7, 4))
+console.log(triArea(10, 10))
