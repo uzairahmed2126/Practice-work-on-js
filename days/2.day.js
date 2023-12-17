a338b82fe9149ad21d8c8d9ce4b46f033cdec7e8
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-12-18 01:17:47 +0530

    problem soving

--- days/2.day.js
+++ days/2.day.js
@@ -46,3 +46,70 @@ function lessThan100(num1, num2) {
 }
 // console.log(lessThan100(22, 15))
 // console.log(lessThan100(83, 34))
+
+// 15 ==> Football Points
+//     Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far. A win receives 3 points, a draw 1 point and a loss 0 points.    
+function footballPoints(wins, draws, losses) {
+    // return wins * 3 + draws * 1 + losses * 0;
+    return (wins * 3) + (draws * 1)
+}
+// console.log(footballPoints(3, 4, 2))
+// console.log(footballPoints(5, 0, 2))
+// console.log(footballPoints(0, 0, 1))
+
+// 16 ==> Are the Numbers Equal?
+//     Create a function that returns true when num1 is equal to num2; otherwise return false.
+function isSameNum(num1, num2) {
+    return num1 === num2;
+}
+// console.log(isSameNum(4, 8))
+// console.log(isSameNum(2, 2))
+// console.log(isSameNum(2, "2"))
+// console.log(isSameNum(0, -0))
+
+
+// 17 return true if number is true
+function isEven(num) { return num % 2 == 0 }
+// console.log(isEven(2))
+// console.log(isEven(3))
+
+// 18 ==> The Farm Problem
+//     In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+//         chickens = 2 legs
+//         cows = 4 legs
+//         pigs = 4 legs
+//     The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+//     animals(2, 3, 5) ➞ 36
+//     animals(1, 2, 3) ➞ 22
+//     animals(5, 2, 8) ➞ 50
+//     Remember that the farmer wants to know the total number of legs and not the total number of animals.
+function animals(chickens, cows, pigs) {
+    return (chickens * 2) + (cows * 4) + (pigs * 4);
+}
+// console.log(animals(2, 3, 5))
+// console.log(animals(1, 2, 3))
+// console.log(animals(5, 2, 8))
+
+// 19 ==> Convert Hours and Minutes into Seconds
+//     Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+function convert(hours, minutes) {
+    return (60 * 60 * hours) + (60 * minutes);
+}
+// console.log(convert(1, 3));
+// console.log(convert(2, 0));
+// console.log(convert(0, 0));
+
+
+// 20 ==> Equality Check
+//     In this challenge, you must verify the equality of two different given parameters: a and b.
+//     Both the value and the type of parameters need to be tested in order to have a matching equality. The possible types of the given parameters are:
+//         Numbers
+//         Strings
+//         Booleans (false or true)
+//         Special values: undefined, null and NaN
+function checkEquality(first, second) {
+    return first === second;
+}
+console.log(checkEquality(1, true));
+console.log(checkEquality(0, "0"));
+console.log(checkEquality(1, 1));
