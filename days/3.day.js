14da07e2195b27c038cf75be9e6e4b5a078f776f
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-12-19 12:47:11 +0530

    .

--- days/3.day.js
+++ days/3.day.js
@@ -120,5 +120,5 @@ function stringInt(int) {
 function dividesEvenly(a, b) {
     return a % b == 0;
 }
-// console.log(dividesEvenly(98, 7))
-// console.log(dividesEvenly(85, 4))
+console.log(dividesEvenly(98, 7))
+console.log(dividesEvenly(85, 4))
