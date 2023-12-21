afdc1e8405016ec62eaa82ac24325f682e8eefcd
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-12-21 13:36:25 +0530

    solving questions

--- days/5.day.js
+++ days/5.day.js
@@ -1,20 +1,85 @@
-// 51 ==> Check String for Spaces
-//     Create a function that returns true if a string contains any spaces.
-function hasSpaces(str) {
-    let result = 0;
-    for (let i = 0; str[i] != undefined; i++) {
-        // if (str[i] === ' ') {
-        //     result = true;
-        // }else {
-        //     result = false;
-        // }
-        result = str
-    }
-    return result;
-    // return str.includes(' ')
+// 41 ==> Fix the Error: Check Whether a Given Number Is Odd
+//     I've written the function isOdd() to check if a given number is odd or not. Unfortunately, the function does not return the correct result for all the inputs. Fix the error.
+function isOdd(num) {
+    // return num % 2 == 0;
+    return !!(num % 2);
 }
-console.log(hasSpaces("hello"))
-console.log(hasSpaces("hello, world"))
-console.log(hasSpaces(" "))
-console.log(hasSpaces(""))
-console.log(hasSpaces(",./!@#"))
+// console.log(isOdd(-5));
+// console.log(isOdd(25));
+// console.log(isOdd(0));
+
+// 42 ==> Flip the Boolean
+//     Due to a programming concept known as truthiness, certain values can be evaluated to (i.e. take the place of) booleans. For example, 1 (or any number other than 0) is often equivalent to true, and 0 is often equivalent to false.
+//     Create a function that returns the opposite of the given boolean, as a number.
+function flipBaloon(value) {
+    return !value;
+    // return Number(!value);
+}
+// console.log(flipBaloon(true));
+// console.log(flipBaloon(false));
+// console.log(flipBaloon(1));
+// console.log(flipBaloon(0));
+
+// 43 ==> Kinetic Energy
+//     Kinetic energy can be calculated with the following formula:
+//     Kinetic energy = (0.5) * m * v^2
+function kineticEnergy(m, v) {
+    const result = (0.5) * m * v ** 2;
+    return result;
+}
+// console.log(kineticEnergy(60, 3));
+// console.log(kineticEnergy(45, 10));
+// console.log(kineticEnergy(63.5, 7.35));
+
+// 44 ==> Name Greeting!
+//     Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function but use an "arrow function".
+const fun = (str) => {
+    return `${"Hello " + str}`;
+}
+// console.log(fun("Gerald"));
+// console.log(fun("Tiffany"));
+// console.log(fun("Ed"));
+
+// 45 ==> To the Power of _____
+//     Create a function that takes a base number and an exponent number and returns the calculation.
+function calculteExponent(baseNum, exponentNum) {
+    return baseNum ** exponentNum;
+}
+// console.log(calculteExponent(5, 5))
+// console.log(calculteExponent(10, 10))
+// console.log(calculteExponent(3, 3))
+
+// 46 ==> Word without First Character
+//     Create a function that takes a word and returns the new word without including the first character.
+function newWord(str) {
+    return str.slice(1);
+}
+// console.log(newWord("apple"))
+// console.log(newWord("cherry"))
+// console.log(newWord("plum"))
+
+// 47 ==> Concatenating Two Integer Arrays
+//     Create a function to concatenate two integer arrays.
+function concat(firstArr, secondArr) {
+    return firstArr.concat(secondArr);
+}
+// console.log(concat([1, 3, 5], [2, 6, 8]));
+// console.log(concat([7, 8], [10, 9, 1, 1, 2]));
+// console.log(concat([4, 5, 1], [3, 3, 3, 3, 3]));
+
+// 48 ==> Find the Index (Part 1)
+//     Create a function that finds the index of a given item. If the item is not present, return -1.
+function search(arr, findVal) {
+    let result = 0;
+    // result = arr.indexOf(findVal)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == findVal) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(search([1, 5, 3], 5));
+console.log(search([9, 8, 3], 3));
+console.log(search([1, 2, 3], 4));
+console.log(search(["hi", "edabit", "fgh", "abc"], "fgh"))
