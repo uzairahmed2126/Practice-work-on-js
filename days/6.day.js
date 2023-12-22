3ad1f27dc80c59c63f5f333affd860acdb58b18e
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-12-22 16:27:13 +0530

    completed day 6 5 question

--- days/6.day.js
+++ days/6.day.js
@@ -53,17 +53,59 @@ function getContainer(products) {
 function numberArgs(...parameters) {
     let a = 0;
     for (let i = 0; i < parameters.length; i++) {
-            a += i;
+        a += i;
     };
     return parameters.length;
 };
-console.log(numberArgs("a", "b", "c"));
-console.log(numberArgs(10, 20, 30, 40, 50));
-console.log(numberArgs('x', 'y'));
-console.log(numberArgs());
+// console.log(numberArgs("a", "b", "c"));
+// console.log(numberArgs(10, 20, 30, 40, 50));
+// console.log(numberArgs('x', 'y'));
+// console.log(numberArgs());
 
 
 // ➞ 3
 // ➞ 5
 // ➞ 2
 // ➞ 0
+
+// 54 ==>  Extract City Facts
+//     Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
+//         name
+//         population
+//         continent
+//         The string should have the following format: 
+//     "X has a population of Y and is situated in Z" 
+//     (where X is the city name, Y is the population and Z is the continent the city is situated in).
+
+
+
+function cityFacts(obj) {
+    if (obj.continent === undefined) {
+        return `${obj.name} has a population of ${obj.population}`;
+    }
+    return `${obj.name} has a population of ${obj.population}  and is situated in ${obj.continent}`;
+}
+// console.log(cityFacts({
+//     name: "Paris",
+//     population: "2,140,526",
+//     continent: "Europe"
+// }));
+// console.log(cityFacts({
+//     name: "Tokyo",
+//     population: "13,929,286",
+//     continent: "Asia"
+// }));
+// console.log(cityFacts({
+//     name: "Tokyo",
+//     population: "13,929,286"
+// }));
+// ➞ "Paris has a population of 2,140,526 and is situated in Europe"
+// ➞ "Tokyo has a population of 13,929,286 and is situated in Asia"
+// ➞ "Tokyo has a population of 13,929,286"
+
+// 55 ==> Volume of a Box
+//     Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box.
+//         volumeOfBox({ width: 2, length: 5, height: 1 }) ➞ 10
+//         volumeOfBox({ width: 4, length: 2, height: 2 }) ➞ 16
+//         volumeOfBox({ width: 2, length: 3, height: 5 }) ➞ 30
+//     Volume is length * width * height.
